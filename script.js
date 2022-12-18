df6842d8131e7f745b506dab07aad126b9e2b9d0
myusron12 <ryusron19@gmail.com>
2022-12-18 12:52:05 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,230 @@
+var vue = new Vue({
+  el: "#app",
+  data: {
+    judul: "Data Produk",
+    // url: 'http://localhost/trainit/vue/crud.php',
+    url: "http://localhost:8000/crud.php",
+    produk: [],
+    cari: "",
+    loading: true,
+    pesan: {
+      teks: null,
+      tipe: null,
+    },
+    modal: "",
+    form: {
+      nama: null,
+      harga: null,
+      berat: null,
+      deskripsi: null,
+      file: null, // menyimpan file upload di modal tambah
+      gambar: null, // menyimpan nama gambar untuk modal detail & edit
+      id: null,
+    },
+  },
+  created() {
+    // ketika web baru di load, langsung jalankan fungsi ambil_produk untuk mengisi array produk diatas yang akan menampilkan data produk di table produk
+    this.ambil_produk();
+  },
+  methods: {
+    changeModal(value) {
+      this.modal = value;
+    },
+    ambil_produk() {
+      // nyalakan loading
+      this.loading = true;
+
+      // ambil data dari API URL dengan parameter aksi == tampil_produk
+      axios
+        .get(this.url, {
+          params: { aksi: "tampil_produk" },
+        })
+        .then((respon) => {
+          console.log(respon);
+
+          // ambil data dari respon axios
+          let data = respon.data;
+
+          // masukan data ke array produk
+          this.produk = data;
+
+          // matikan loading
+          this.loading = false;
+        })
+        .catch((error) => {
+          // tangkap error jika ada
+          console.log(error);
+        });
+    },
+    handleUpload() {
+      if (this.modal == "modal-tambah") {
+        this.form.file = this.$refs.file_tambah.files[0];
+      } else if (this.modal == "modal-ubah") {
+        this.form.file = this.$refs.file_ubah.files[0];
+      }
+    },
+    isi_form(produk) {
+      console.log(produk);
+
+      this.form.id = produk.id;
+      this.form.nama = produk.nama;
+      this.form.harga = produk.harga;
+      this.form.berat = produk.berat;
+      this.form.gambar = produk.gambar;
+      this.form.deskripsi = produk.deskripsi;
+    },
+    tambah_produk() {
+      // disable semua tombol submit
+      $("button[type=submit]").prop("disabled", true);
+
+      // siapkan data tambah dari obj formData
+      let dataTambah = new FormData();
+
+      dataTambah.append("aksi", "tambah_produk");
+      dataTambah.append("nama", this.form.nama);
+      dataTambah.append("harga", this.form.harga);
+      dataTambah.append("berat", this.form.berat);
+      dataTambah.append("deskripsi", this.form.deskripsi);
+      dataTambah.append("file", this.form.file);
+
+      // jalankan method axios post sambil melemparkan data_tambah ke API URL
+      axios
+        .post(this.url, dataTambah)
+        .then((respon) => {
+          // tampung respon.data
+          let data = respon.data;
+
+          console.log(data);
+
+          // jika respon sukses ==true
+          if (data.sukses) {
+            // sembunyikan/tutup modal-tambah
+            $("#modal-tambah").modal("hide");
+
+            // nyalakan tombol submit
+            $("button[type=submit]").prop("disabled", false);
+
+            // bersikan form
+            this.resetForm;
+
+            // jalankan fungsi ambil produk untuk merefresh table data produk
+            this.ambil_produk();
+
+            // isi teks pesan dari respon untuk menampilkan alert
+            this.pesan.teks = data.pesan;
+            // isi tipe pesan dari respon
+            this.pesan.tipe = data.tipe;
+          }
+        })
+        .catch((error) => {
+          // tangkap error jika ada
+          console.log(error);
+        });
+    },
+    ubah_produk() {
+      // disable semua tombol submit
+      $("button[type=submit]").prop("disabled", true);
+
+      // siapkan data ubah dari obj formData
+      let dataUbah = new FormData();
+
+      dataUbah.append("aksi", "ubah_produk");
+      dataUbah.append("id_produk", this.form.id);
+      dataUbah.append("nama", this.form.nama);
+      dataUbah.append("harga", this.form.harga);
+      dataUbah.append("berat", this.form.berat);
+      dataUbah.append("deskripsi", this.form.deskripsi);
+      dataUbah.append("file", this.form.file);
+
+      // jalankan method axios post sambil melemparkan data ke API URL
+      axios
+        .post(this.url, dataUbah)
+        .then((respon) => {
+          // sembunyikan/tutup modal-ubah
+          $("#modal-ubah").modal("hide");
+
+          // nyalakan tombol submit
+          $("button[type=submit]").prop("disabled", false);
+
+          // tampung respon.data
+          let data = respon.data;
+
+          console.log(data);
+
+          // jika respon sukses ==true
+          if (data.sukses) {
+            // jalankan fungsi ambil produk untuk merefresh table data produk
+            this.ambil_produk();
+          }
+
+          // isi teks pesan dari respon untuk menampilkan alert
+          this.pesan.teks = data.pesan;
+          // isi tipe pesan dari respon
+          this.pesan.tipe = data.tipe;
+
+          // bersihkan isi form
+          this.resetForm;
+        })
+        .catch((error) => {
+          // tangkap error jika ada
+          console.log(error);
+        });
+    },
+    hapus_produk(id_produk) {
+      // konfirmasi user apakah yakin ingin menghapus data
+      var konfirmasi = confirm("Apakah anda yakin?");
+
+      // jika iyaa
+      if (konfirmasi) {
+        // siapkan data hapus dari obj formData
+        let dataHapus = new FormData();
+
+        dataHapus.append("aksi", "hapus_produk");
+        dataHapus.append("id_produk", id_produk);
+
+        // jalankan method axios post sambil melemparkan data ke API URL
+        axios
+          .post(this.url, dataHapus)
+          .then((respon) => {
+            // tampung respon.data
+            let data = respon.data;
+
+            console.log(data);
+
+            // jika respon sukses ==true
+            if (data.sukses) {
+              // jalankan fungsi ambil produk untuk merefresh table data produk
+              this.ambil_produk();
+
+              // isi teks pesan dari respon untuk menampilkan alert
+              this.pesan.teks = data.pesan;
+              // isi tipe pesan dari respon
+              this.pesan.tipe = data.tipe;
+            } else {
+              this.pesan.teks = data.pesan;
+              this.pesan.tipe = data.tipe;
+            }
+          })
+          .catch((error) => {
+            // tangkap error jika ada
+            console.log(error);
+          });
+      }
+    },
+    resetForm() {
+      var self = this;
+      Object.keys(this.form).forEach(function (key, index) {
+        self.form[key] = "";
+      });
+    },
+  },
+  filters: {
+    formatNomor(value) {
+      if (value == null) {
+        return 0;
+      } else {
+        return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+      }
+    },
+  },
+});
